10f9f5a0f711cc0138d0dc22266a2794c5e5ef0b
Garosan <garosanchez27@gmail.com>
2025-08-28 21:31:47 -0600

    Reuse ticket-item component for ticket detail page

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Placeholder } from "@/components/placeholder";
 import { Button } from "@/components/ui/button";
 import { initialTickets } from "@/data";
+import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { ticketsPath } from "@/paths";
 
 type TicketPageProps = {
@@ -21,7 +22,7 @@ const TicketPage = async ({ params }: TicketPageProps) => {
         label="Ticket not found"
         button={
           <Button asChild variant="outline">
-            <Link href={ticketsPath()}>Go back to tickets</Link>
+            <Link href={ticketsPath()}>Go to Tickets</Link>
           </Button>
         }
       />
@@ -29,9 +30,8 @@ const TicketPage = async ({ params }: TicketPageProps) => {
   }
 
   return (
-    <div>
-      <h2 className="text-lg">{ticket.title}</h2>
-      <p className="text-sm">{ticket.content}</p>
+    <div className="flex justify-center animate-fade-from-top">
+      <TicketItem ticket={ticket} isDetail />
     </div>
   );
 };
