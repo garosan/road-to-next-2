62a85437b064cb344460976a3576e47441213bb7
Garosan <garosanchez27@gmail.com>
2025-09-06 17:24:55 -0600

    Add premature optimization changes and action callbacks but with build errors

--- src/components/form/utils/to-action-state.ts
+++ src/components/form/utils/to-action-state.ts
@@ -1,9 +1,17 @@
 import { ZodError } from "zod";
 
 export type ActionState = {
+  status?: "SUCCESS" | "ERROR";
   message: string;
   payload?: FormData;
   fieldErrors: Record<string, string[] | undefined>;
+  timestamp: number;
+};
+
+export const EMPTY_ACTION_STATE: ActionState = {
+  message: "",
+  fieldErrors: {},
+  timestamp: Date.now(),
 };
 
 export const fromErrorToActionState = (
@@ -14,25 +22,38 @@ export const fromErrorToActionState = (
     // if validation error with Zod, return first error message
 
     return {
+      status: "ERROR",
       message: error.issues[0].message,
       fieldErrors: error.flatten().fieldErrors,
       payload: formData,
+      timestamp: Date.now(),
     };
   } else if (error instanceof Error) {
     // if another error instance, return error message
     // e.g. database error
     return {
+      status: "ERROR",
       message: error.message,
       fieldErrors: {},
       payload: formData,
+      timestamp: Date.now(),
     };
   } else {
     // if not an error instance but something else crashed
     // return generic error message
     return {
-      message: "An unknown error occurred",
+      status: "ERROR",
+      message: "An unknown error occured",
       fieldErrors: {},
       payload: formData,
+      timestamp: Date.now(),
     };
   }
+};
+
+export const toActionState = (
+  status: ActionState["status"],
+  message: string
+): ActionState => {
+  return { status, message, fieldErrors: {}, timestamp: Date.now() };
 };
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -3,7 +3,9 @@
 import { Ticket } from "@prisma/client";
 import { useActionState } from "react";
 import { FieldError } from "@/components/form/field-error";
+import { useActionFeedback } from "@/components/form/hooks/use-action-feedback";
 import { SubmitButton } from "@/components/form/submit-button";
+import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
@@ -16,7 +18,7 @@ type TicketUpsertFormProps = {
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
   const [actionState, action] = useActionState(
     upsertTicket.bind(null, ticket?.id),
-    { message: "", fieldErrors: {} }
+    EMPTY_ACTION_STATE
   );
 
   return (
@@ -43,8 +45,6 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
       <FieldError actionState={actionState} name="content" />
 
       <SubmitButton label={ticket ? "Edit" : "Create"} />
-
-      {actionState.message}
     </form>
   );
 };
